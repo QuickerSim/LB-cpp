8ea94a655da3b2e5279b12088506ab0c2af689b7
Konrad Jabłoński <k.jablonski@soyincode.pl>
2022-07-27 14:21:49 +0200

    cleanup and added readme

---
+++ README.md
@@ -0,0 +1,8 @@
+Repozytorium zawiera implementację metody Lattice-Bolztmann dla przypadku Cavity na niejednorodnej siatce w języku `C++`.
+
+Aby skompilowac program nalezy skorzystac z oprogramowania CMake, przykładowo:
+`cmake -S. -Bbuild`
+a następnie
+`cmake --build ./build`
+
+W celu uruchomienia programu nalezy uruchomic plik binary o nazwie `lb`
--- src/main.cpp
+++ src/main.cpp
@@ -3,66 +3,66 @@
 
 int main()
 {
-    QuadTree root(0.0, 1.0, 0.0, 1.0);
+	QuadTree root(0.0, 1.0, 0.0, 1.0);
 
-    std::vector<Point> points;
+	std::vector<Point> points;
 
-//		points.push_back(Point(0.49, 0.51));
-//		points.push_back(Point(0.51, 0.51));
-//		points.push_back(Point(0.49, 0.49));
-//		points.push_back(Point(0.51, 0.49));
-//		points.push_back(Point(0, 1));
-//		points.push_back(Point(0, 0.1));
-//		points.push_back(Point(0, 0.2));
-//		points.push_back(Point(0, 0.3));
-//		points.push_back(Point(0, 0.4));
-//		points.push_back(Point(0, 0.5));
-//		points.push_back(Point(0, 0.6));
-//		points.push_back(Point(0, 0.7));
-//		points.push_back(Point(0, 0.8));
-//		points.push_back(Point(0, 0.9));
-//		points.push_back(Point(0, 1.0));
+	//		points.push_back(Point(0.49, 0.51));
+	//		points.push_back(Point(0.51, 0.51));
+	//		points.push_back(Point(0.49, 0.49));
+	//		points.push_back(Point(0.51, 0.49));
+	//		points.push_back(Point(0, 1));
+	//		points.push_back(Point(0, 0.1));
+	//		points.push_back(Point(0, 0.2));
+	//		points.push_back(Point(0, 0.3));
+	//		points.push_back(Point(0, 0.4));
+	//		points.push_back(Point(0, 0.5));
+	//		points.push_back(Point(0, 0.6));
+	//		points.push_back(Point(0, 0.7));
+	//		points.push_back(Point(0, 0.8));
+	//		points.push_back(Point(0, 0.9));
+	//		points.push_back(Point(0, 1.0));
 
-//		points.push_back(Point(1, 1));
-//		points.push_back(Point(1, 0.1));
-//		points.push_back(Point(1, 0.2));
-//		points.push_back(Point(1, 0.3));
-//		points.push_back(Point(1, 0.4));
-//		points.push_back(Point(1, 0.5));
-//		points.push_back(Point(1, 0.6));
-//		points.push_back(Point(1, 0.7));
-//		points.push_back(Point(1, 0.8));
-//		points.push_back(Point(1, 0.9));
-//		points.push_back(Point(1, 1.0));
+	//		points.push_back(Point(1, 1));
+	//		points.push_back(Point(1, 0.1));
+	//		points.push_back(Point(1, 0.2));
+	//		points.push_back(Point(1, 0.3));
+	//		points.push_back(Point(1, 0.4));
+	//		points.push_back(Point(1, 0.5));
+	//		points.push_back(Point(1, 0.6));
+	//		points.push_back(Point(1, 0.7));
+	//		points.push_back(Point(1, 0.8));
+	//		points.push_back(Point(1, 0.9));
+	//		points.push_back(Point(1, 1.0));
 
-		points.push_back(Point(0.35, 0.56));
-		points.push_back(Point(0.25, 0.56));
+	points.push_back(Point(0.35, 0.56));
+	points.push_back(Point(0.25, 0.56));
 
-		points.push_back(Point(0.25, 0.46));
-		points.push_back(Point(0.35, 0.46));
+	points.push_back(Point(0.25, 0.46));
+	points.push_back(Point(0.35, 0.46));
 
-//		points.push_back(Point(0.251, 1));
-//		points.push_back(Point(0.249, 1));
-//		points.push_back(Point(0.751, 1));
-//		points.push_back(Point(0.749, 1));
+	//		points.push_back(Point(0.251, 1));
+	//		points.push_back(Point(0.249, 1));
+	//		points.push_back(Point(0.751, 1));
+	//		points.push_back(Point(0.749, 1));
 
-		// root.CreateTreeByPointCount(points);
-		root.CreateTreeAtPoints(points, 5);
+	// root.CreateTreeByPointCount(points);
+	root.CreateTreeAtPoints(points, 5);
 
-    root.BalanceTree(1);
+	root.BalanceTree(1);
 
-    MPIBlock dummyblock;
+	MPIBlock dummyblock;
 
-    root.PopulateMpiBlock(dummyblock);
+	root.PopulateMpiBlock(dummyblock);
 
-		QSDouble omega = 1.0 / 0.51;
+	QSDouble omega = 1.0 / 0.51;
 
-		dummyblock.InitNodes(32, 32, 2, omega, 0.1, 0.0);
+	dummyblock.InitNodes(32, 32, 2, omega, 0.1, 0.0);
 
-		QSSize maxtimestep = 10000;
-		QSSize sizeinterval = 1000;
+	QSSize maxtimestep = 10000;
+	QSSize sizeinterval = 100;
 
-    dummyblock.Simulate(maxtimestep, sizeinterval);
+	dummyblock.Simulate(maxtimestep, sizeinterval);
 
-    return 0;
+	return 0;
 }
